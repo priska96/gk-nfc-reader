07161d08e62543eacb7e8fd750bc2fda4d17540d
Priska <kohnen@sciendis.de>
2024-09-25 14:37:27 +0200

    added openssl

---
+++ ios/OpenSSL.framework/Headers/shim.h
@@ -0,0 +1,24 @@
+#ifndef COpenSSL_H
+#define COpenSSL_H
+
+#include "define_wrapper.h"
+#include <openssl/cms.h>
+#include <openssl/conf.h>
+#include <openssl/bn.h>
+#include <openssl/cmac.h>
+#include <openssl/evp.h>
+#include <openssl/ec.h>
+#include <openssl/err.h>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/md4.h>
+#include <openssl/md5.h>
+#include <openssl/sha.h>
+#include <openssl/hmac.h>
+#include <openssl/rand.h>
+#include <openssl/param_build.h>
+#include <openssl/pkcs12.h>
+#include <openssl/ocsp.h>
+#include <openssl/x509v3.h>
+
+#endif

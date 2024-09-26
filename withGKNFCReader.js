14b9671b7e128b6ef818be2048a977bad51feb54
Priska <kohnen@sciendis.de>
2024-09-26 08:15:25 +0200

    added expo config plugin with info plidst, entitlements

---
+++ withGKNFCReader.js
@@ -0,0 +1,46 @@
+const {
+  withInfoPlist,
+  withEntitlementsPlist,
+  createRunOncePlugin,
+} = require('@expo/config-plugins');
+
+const withGKNFCReader = config => {
+  // iOS Configuration
+  config = withInfoPlist(config, config => {
+    config.modResults['NFCReaderUsageDescription'] = 'NFC Test';
+    config.modResults[
+      'com.apple.developer.nfc.readersession.iso7816.select-identifiers'
+    ] = [
+      'D2760001448000',
+      'D27600014601',
+      'D27600014606',
+      'D27600000102',
+      'A000000167455349474E',
+      'D27600006601',
+      'D27600014602',
+      'E828BD080FA000000167455349474E',
+      'E828BD080FD27600006601',
+      'D27600014603',
+      'A0000002471001',
+      'A00000030800001000',
+    ];
+    return config;
+  });
+
+  config = withEntitlementsPlist(config, config => {
+    // Modify the entitlements plist here
+    config.modResults['com.apple.developer.nfc.readersession.formats'] = [
+      'TAG',
+    ];
+
+    return config;
+  });
+  return config;
+};
+
+// Plugin metadata to avoid re-running the plugin multiple times
+module.exports = createRunOncePlugin(
+  withGKNFCReader,
+  'with-gk-nfc-reader',
+  '1.0.8',
+);
